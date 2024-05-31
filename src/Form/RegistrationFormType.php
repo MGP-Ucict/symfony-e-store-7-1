a5079da26aadcc313a5df71d1d5c6dac5090639d
MGP-Ucict <penelova.maria@gmail.com>
2024-05-31 20:25:08 +0300

    Register Page styled

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -17,9 +18,15 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+			->add('email', EmailType::class, [
+				'label' => 'Email',
+				'label_attr' => ['class' => 'col-md-4 col-form-label text-md-end'],
+				'attr' => ['class' => 'form-control']
+			])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
+				'label_attr' => ['class' => 'col-md-4 form-check-label'],
+				'attr' => ['class' => 'form-control form-check-input'],
                 'constraints' => [
                     new IsTrue([
                         'message' => 'You should agree to our terms.',
@@ -27,10 +34,12 @@ class RegistrationFormType extends AbstractType
                 ],
             ])
             ->add('plainPassword', PasswordType::class, [
+				'label' => 'Password',
+				'label_attr' => ['class' => 'col-md-4 col-form-label text-md-end'],
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password','class' => 'form-control'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
